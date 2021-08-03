c4580a6485aada93e6710efe6075617b7d95d860
Kevin <k.j.christensen93@gmail.com>
2021-08-03 10:53:45 -0600

    Add testing_data.py
    Testing data creates the files that are used by the trained model to
    produce prediction files

---
+++ testing_data.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import ee
+ee.Initialize()
+
+import pandas as pd
+import configparser
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('aisconfig.ini')
+
+    
+    state = config["WHENWHERE"]["STATE"]
+    state_abbrev = config["WHENWHERE"]["STATE_ABBREVIATION"]
+    start_year = int(config['WHENWHERE']['START_YEAR'])
+    end_year = int(config['WHENWHERE']['END_YEAR'])
+    covariate_folder = config["GEEPATHS"]["ASSETID"]
+    output_path = config["LOCALPATHS"]["DECADE1"] 
+
+    HUC_clip = ee.FeatureCollection("USGS/WBD/2017/HUC12").filter(ee.Filter.eq('states',state_abbrev))
+    years = range(start_year, end_year)
+    images = list(map(lambda x: ee.Image(covariate_folder + "covariates" + str(x)), years))
+
+
+
+    mean_image = ee.Image(ee.ImageCollection.fromImages(images).mean())
+
+    huc_level_covariates = mean_image.reduceRegions(**{
+        'collection': HUC_clip,
+        'reducer': ee.Reducer.mean(),
+        'crs': 'EPSG:4326',
+        'scale': 100,
+        'tileScale': 16})
+
+    pd.DataFrame([x['properties'] for x in huc_level_covariates.getInfo()['features']]).to_csv(output_path)
+
+    print(".csvs written to ", output_path)
+
+
+if __name__ == "__main__":
+    main()
